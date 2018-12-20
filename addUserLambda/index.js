9760172e28e38039cdf96f9258d379ae2fe172aa
Jonathan Spruance <jspruance@inflightcorp.com>
2018-12-19 21:49:02 -0800

    Renaming lambdas and dynamo table

---
+++ addUserLambda/index.js
@@ -0,0 +1,40 @@
+"use strict";
+const AWS = require("aws-sdk");
+
+AWS.config.update({ region: "us-west-2" });
+
+exports.handler = async (event, context) => {
+  const ddb = new AWS.DynamoDB({ apiVersion: "2012-10-08" });
+
+  let requestBody = JSON.parse(event.body);
+  let responseBody = "";
+  let statusCode = 0;
+
+  const params = {
+    TableName: "Users",
+    Item: {
+      id: { S: requestBody.id }
+    }
+  };
+
+  // Call DynamoDB to add the item to the table
+  try {
+    const data = await ddb.putItem(params).promise();
+    responseBody = JSON.stringify(data);
+    statusCode = 201;
+  } catch (err) {
+    responseBody = `Unable to put locations. Error: ${err}`;
+    statusCode = 403;
+  }
+
+  const response = {
+    statusCode: statusCode,
+    headers: {
+      my_header: "my_value"
+    },
+    body: responseBody,
+    isBase64Encoded: false
+  };
+
+  return response;
+};
